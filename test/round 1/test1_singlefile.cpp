6cf96153483cc479edd8925bd1944e88a26f09d5
lzcxt <veckoper@gmail.com>
2022-06-29 08:52:52 +0800

    add test

---
+++ test/round 1/test1_singlefile.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief A's discription
+ * 
+ */
+class A {
+public:
+    void A_method_1() {
+
+    }
+    void A_method_2() {
+
+    }
+    void A_function_1() {
+
+    }
+private:
+    int A_int;
+    double A_double;
+};
+
+class B : A {  // no discription
+public:
+    void B_method_1() {
+
+    }
+    void B_method_2() {
+
+    }
+    void B_function_1() {
+
+    }
+private:
+    int B_int;
+    double B_double;
+};
+
+class C {
+
+};
+
+class D {
+
+};
+
+void f() {
+    
+}
+int main() {
+    return 0;
+}
