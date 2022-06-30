d7f89484d447c7db9d1378d4b44bc8392893bc76
lzcxt <veckoper@gmail.com>
2022-06-30 16:11:11 +0800

    finish file building

---
+++ myCppDoc/code/model/classRelationGenerator.cpp
@@ -0,0 +1,11 @@
+#include "model/classRelationGenerator.h"
+using namespace std;
+
+void ClassRelationGenerator::pushBack(string str) {
+	/* not implemented yet */
+}
+
+list<Class> ClassRelationGenerator::getClasses() {
+	/* not implemented yet */
+	return list<Class>();
+}
